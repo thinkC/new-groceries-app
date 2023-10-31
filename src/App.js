77009c4c0a106aa6c65a37a989ccb130a7135f72
thinkc <tunde.oyewo@gmail.com>
2023-10-31 18:35:43 +0000

    updated file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
-import Top from './components/Top';
-import GroceryList from './components/GroceryList';
-import AddGrocery from './components/AddGrocery';
-import EditGrocery from './components/EditGrocery';
-import './App.css';
+import React, { Component } from 'react'
+import QuoteAndAuthor from './components/QuoteAndAuthor';
+import quotes from './QuoteDB';
 
+export default class App extends Component {
 
-class App extends Component {
+  //state
+  state = {
+    quote: quotes[0].quote,
+    author: quotes[0].author
+  }
+
+  //generate diffrent quote function
+  generateRandomQuote = (arr) => {
+    //get random numbers
+    let num = Math.floor(Math.random() * quotes.length)
+
+    let newQuote = quotes[num];
+
+    //update state
+    this.setState({
+      quote: newQuote.quote,
+      author: newQuote.author
+    })
+
+    this.shuffleQuotes(quotes)
+
+  }
+
+  //shuufle quotes function
+  shuffleQuotes = (arr) => {
+    return arr.sort(function () { return 0.5 - Math.random() });
+  }
 
   render() {
-
     return (
-      <React.Fragment>
-        <Top />
-        <Switch>
-          <Route exact path="/" component={GroceryList} />
-          <Route path="/add" component={AddGrocery} />
-          <Route path="/edit/:id" component={EditGrocery} />
-        </Switch>
-
-      </React.Fragment>
+      <div className="container">
+        <h1 className="text-center">Random Quote</h1>
+        <QuoteAndAuthor
+          generateRandomQuote={this.generateRandomQuote}
+          quote={this.state}
+        />
+      </div>
     )
   }
 }
-
-
-export default App;
---
+++ src/QuoteDB.js
@@ -0,0 +1,119 @@
+const quotes = [
+    {
+        "quote": "Forget all the reasons why it won't work and believe the one reason why it will",
+        "author": "Unknown"
+    },
+    {
+        "quote": "Always do what you are afraid to do",
+        "author": "Ralph Waldo Emerson"
+    },
+
+    {
+        "quote": "Don’t be intimidated by what you don’t know. That can be your greatest strength and ensure that you do things differently from everyone else.",
+        "author": "Sara Blakely"
+    },
+    {
+        "quote": "If you keep on doing what you've always done, you will keep getting what you've always gotten.",
+        "author": "Unknown"
+    },
+    {
+        "quote": " For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. John 3:16",
+        "author": "Jesus Christ"
+    },
+    {
+        "quote": "The surest way to find your dream job is to create it.",
+        "author": "Unknown"
+
+    },
+    {
+        "quote": "You are confined only by the walls you build yourself.",
+        "author": "Andrew Murphy"
+    },
+    {
+        "quote": "Today's accomplishments were yesterday's impossibilities.",
+        "author": "Robert Schuller"
+    },
+    {
+        "quote": "You don't have to see the whole staircase. Just take the first step.",
+        "author": "Martin Luther King"
+    },
+    {
+        "quote": "How many success stories do you need to hear before you make your own?",
+        "author": "Unknown"
+    },
+    {
+        "quote": "Watch carefully the magic that occurs when you give a person just enough comfort to be themselves",
+        "author": "Atticus"
+    },
+
+    {
+        "quote": "You never know how strong you are, until being strong is your only choice",
+        "author": "Bob Marley"
+    },
+    {
+        "quote": "All great achievements require time",
+        "author": "Maya Angelou"
+    },
+    {
+        "quote": "When you have exhausted all possibilities, remember this: you haven't.",
+        "author": "Thomas Edison"
+    },
+    {
+        "quote": "Continuous improvement is better than delayed perfection",
+        "author": "Mark Twain"
+    },
+    {
+        "quote": "And the God of all grace, who called you to his eternal glory in Christ, after you have suffered a little while, will himself restore you and make you strong, firm and steadfast.",
+        "author": "IPeter 5:10"
+    },
+    {
+        "quote": "A I can do all things through Christ who strengthens me.",
+        "author": "Philipians4:13"
+    },
+    {
+        "quote": "Time you enjoy wasting is not wasted time",
+        "author": "John Lennon"
+    },
+    {
+        "quote": "If you want to lift yourself up, lift up someone else",
+        "author": "Booker T. Washington"
+    },
+    {
+        "quote": "Acknowledging the good that you already have in your life is the foundation for all abundance",
+        "author": "Eckhart Tolle"
+    },
+    {
+        "quote": "If you want to lift yourself up, lift up someone else",
+        "author": "Booker T. Washington"
+    },
+    {
+        "quote": "Muddy water is best cleared by leaving it alone.",
+        "author": "Anaïs Nin"
+    },
+    {
+        "quote": "There is nothing so useless as doing efficiently that which should not be done at all.",
+        "author": "Peter Drucker"
+    },
+    {
+        "quote": "Strength does not come from winning. Your struggles develop your strengths",
+        "author": "Arnold Schwarzenegger"
+    },
+    {
+        "quote": "Whenever you are confronted with an opponent, conquer them with love",
+        "author": "Mahatma Gandhi"
+    },
+    {
+        "quote": "Enjoy the little things, for one day you may look back and realize they were the big things",
+        "author": "Robert Brault"
+    },
+    {
+        "quote": "Do something today that your future self will thank you for",
+        "author": "Sean Patrick Flanery"
+    },
+    {
+        "quote": "It is not the load that breaks you down. It's the way you carry it",
+        "author": "Lou Holtz"
+    },
+]
+
+export default quotes;
